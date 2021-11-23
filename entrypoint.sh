b20e8c3fd9befc75abf4837707fc6c572993a79c
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2021-11-23 15:55:44 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -l
+set -e
+all_args=$@
+echo "All arg = ${all_args}" 
+echo "Hello $2"
+echo "Var1 = $4"
+echo "Var2 = $6"
+echo "Var3 = $8"
+time="$(date) - From Repo"
+echo "::set-output name=time::$time"
+whoami
+env
+pwd
+ls -la
